7fad19771986158708dbcff437035e64e5645cc5
Nathan Mittelette <nathan.mittelette@axopen.com>
2021-01-04 17:01:33 +0100

    Traitement des incidents sans truck uniquement

--- src/main/java/fr/dreamteam/manager/service/IncidentToProcess.java
+++ src/main/java/fr/dreamteam/manager/service/IncidentToProcess.java
@@ -5,12 +5,15 @@ import fr.dreamteam.manager.model.Truck;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class IncidentToProcess implements IIncidentToProcess {
 
     @Override
     public List<Incident> getIncidentsToProcess(List<Incident> incidents, List<Truck> trucks) {
-        return incidents;
+
+        // Si jamais un incident est déjà associé à un truck alors il n'est pas a traiter.
+        return incidents.stream().filter(i -> trucks.stream().noneMatch(t -> t.getIncidents().stream().anyMatch(it -> it.getId() == i.getId()))).collect(Collectors.toList());
     }
 }
